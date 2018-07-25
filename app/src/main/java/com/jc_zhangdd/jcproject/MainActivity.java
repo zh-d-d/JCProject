3d73ad39134b7296243c13aeea15c70529663bef
zhangdd <86431843@qq.com>
2018-07-25 11:30:30 +0800

    feat:configure glide library
    Signed-off-by: zhangdd <86431843@qq.com>

--- app/src/main/java/com/jc_zhangdd/jcproject/MainActivity.java
+++ app/src/main/java/com/jc_zhangdd/jcproject/MainActivity.java
@@ -2,12 +2,20 @@ package com.jc_zhangdd.jcproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.jc_zhangdd.lib_common.glide.GlideApp;
+
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    private ImageView viewById;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewById = ((ImageView) findViewById(R.id.iv));
+        GlideApp.with(this).load("https://www.baidu.com/img/bd_logo1.png?where=super").into(viewById);
     }
 }
---
+++ module_common/src/main/java/com/jc_zhangdd/lib_common/glide/JCAppGlideModule.java
@@ -0,0 +1,14 @@
+package com.jc_zhangdd.lib_common.glide;
+
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Project: JCProject
+ * Created by Zdd on 2018/7/21.
+ */
+
+@GlideModule
+public class JCAppGlideModule extends AppGlideModule {
+}
